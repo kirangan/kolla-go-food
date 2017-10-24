eec64673105e564aacd8652531b8c92677e72478
kirangan <qurin.ainun@gmail.com>
2017-10-24 13:51:42 +0700

    Add data seed & edit scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Food.delete_all
+
+Food.create!(
+	name: "Tenderloin Steak",
+	description:
+	%{<p>
+		<em>Daging terbaik dari Setiap Sapi</em>
+		Tenderloin hanya terdapat di bagian tengah sapi. Daging tenderloin 
+    adalah bagian daging untuk steak yang paling mahal dibandingkan dengan daging lainnya. Tenderloin 
+    disajikan dengan ukuran daging yang lebih kecil daripada daging jenis lainnya, misalnya sirloin.
+		</p>},
+	image_url: "Tenderloin.jpg",
+	price: 95000.00
+	)
+
+Food.create!(
+  name: "Rawon",
+  description:
+  %{<p>
+    <em>Si Hitam Manis, Rawon Daging Sapi Si Hitam Manis, Rawon Daging Sapi </em>
+    Rawon merupakan salah satu sajian kuliner masakan yang sangat khas di Indonesia 
+    khususnya di daerah Surabaya. Rawon ini memiliki cita rasa yang sangat gurih dan lezat 
+    sehingga memiliki banyak sekali penggemar yang kebanyakan dari kalangan anak muda dan juga 
+    dari kalangan orang tua. Rawon terbuat dari bahan dasar Daging Sapi yang diberi 
+    bumbu kluwek sehingga warna bumbu cenderung kehitaman.
+    </p>},
+    image_url: "Rawon.jpeg",
+    price: 20000.00
+  )
+
+Food.create!(
+  name: "Soto",
+  description:
+  %{<p>
+    <em>Soto Enak</em>
+    Soto, sroto, sauto, tauto, atau coto adalah makanan khas Indonesia seperti sop yang terbuat 
+    dari kaldu daging dan sayuran. Daging yang paling sering digunakan adalah daging sapi dan aya
+    </p>},
+    image_url: "Soto.jpg",
+    price: 30000.00
+  )
