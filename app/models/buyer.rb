f0a73089da6aeab5e021943e3b94131294fa5945
kirangan <qurin.ainun@gmail.com>
2017-10-24 17:13:06 +0700

    add validate presence

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,11 @@
 class Buyer < ApplicationRecord
+	validates :name, :phone, :email, :address, presence: true
+	validates :email, uniqueness: true, format: {
+		with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+		message: 'must be valid email format'
+	}
+	validates :phone, format: { 
+		with: /\d{12}/,
+		message: 'phone format is invalid'
+	}
 end
