8dc8b788fd173fdcf8b4468bad4d13f3d8015a9f
kirangan <qurin.ainun@gmail.com>
2017-10-25 08:59:17 +0700

    fix buyer conflict

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -4,13 +4,5 @@ class Buyer < ApplicationRecord
 		with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
 		message: 'must be valid email format'
 	}
-<<<<<<< HEAD
 	validates :phone, numericality: true, length: { in: 9..12 } 
-	
-=======
-	validates :phone, format: { 
-		with: /\d{12}/,
-		message: 'phone format is invalid'
-	}
->>>>>>> master
 end
