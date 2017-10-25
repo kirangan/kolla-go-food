f9692bb6eb1d1a3963b537120e67058ab6bf3dcd
kirangan <qurin.ainun@gmail.com>
2017-10-25 10:47:36 +0700

    edit to make rspec DRY with describe in describe

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -45,42 +45,35 @@ describe Food do
     expect(food2.errors[:name]).to include("has already been taken")
   end
 
-  it "returns a sorted array of results that match" do
-    food1 = Food.create(
+  describe "filter name by letter" do 
+    before :each do 
+      @food1 = Food.create(
       name: "Nasi uduk",
       description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
       price: 10000.0
       )
-    food2 = Food.create(
+      @food2 = Food.create(
       name: "Kerak Telor",
       description: "Betawi traditional spicy omelette made from glutinous rice cooked bla bla bla",
       price: 8000.0
       )
-    food3 = Food.create(
+      @food3 = Food.create(
       name: "Nasi Semur Jengkol",
       description: "Based on dongfruit, this menu promises i don't know ",
       price: 8000.0
       )
-    expect(Food.by_letter("N")).to eq([food3, food1])
-  end
+    end
 
-  it "omits results that do not match" do
-    food1 = Food.create(
-      name: "Nasi uduk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
-      price: 10000.0
-      )
-    food2 = Food.create(
-      name: "Kerak Telor",
-      description: "Betawi traditional spicy omelette made from glutinous rice cooked bla bla bla",
-      price: 8000.0
-      )
-    food3 = Food.create(
-      name: "Nasi Semur Jengkol",
-      description: "Based on dongfruit, this menu promises i don't know ",
-      price: 8000.0
-      )
-    expect(Food.by_letter("N")).not_to eq(food2)
-  end
+    context "with matching letter" do
+      it "returns a sorted array of results that match" do
+        expect(Food.by_letter("N")).to eq([@food3, @food1])
+      end
+    end
 
+    context "with non-matching letter" do
+      it "omits results that do not match" do
+        expect(Food.by_letter("N")).not_to eq(@food2)
+      end
+    end
+  end
 end
