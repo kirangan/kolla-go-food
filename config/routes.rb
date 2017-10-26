fb48731cc98dfc773aaca9723ad726a39b7a8e48
kirangan <qurin.ainun@gmail.com>
2017-10-26 15:34:48 +0700

    view store

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'store/index'
-
-  resources :buyers
+  root 'store#index', as: 'store_index'
   resources :foods
-  resources :stores
-  get 'home/goodbye'
-
   get 'home/hello'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
