6c6ef72d8e84a5f2dedd9a0d1e7574a5cd8d3a38
kirangan <arekayu12@gmail.com>
2017-11-02 17:06:28 +0700

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  get 'home/hello'
+  root 'store#index', as: 'store_index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :carts
   resources :line_items
   resources :categories
   resources :orders
   resources :foods
   resources :users
-  root 'store#index', as: 'store_index'
-  get 'home/hello'
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
